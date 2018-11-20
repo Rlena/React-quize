2424ce1fb78b8cfbfb948423010fadc859b714c3
Rlena <elena1902881@gmail.com>
2018-11-20 16:08:15 +0300

    Added Redux for app

--- src/index.js
+++ src/index.js
@@ -2,13 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './store/reducers/rootReducer'
+import { createStore, compose, applyMiddleware } from 'redux' // создание store, compose для redux-devtools
+import { Provider } from 'react-redux' // для поддержки redux в приложении
+import thunk from 'redux-thunk' // для асинхронных actions
 
+// подключаем возможность использовать redux-devtools (док. redux-devtools, 1.2 Advanced store setup)
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+// формирование store
+// он определяетя функцией createStore, в кот. передаем rootReducer, в котором собраны все reducer
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk) // thunk передаем как middleware
+  )
+)
+
+// обернем приложение в Provider, как параметр передадим store, который только что создали
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+// rootReducer объединяет все существующие в приложении reducers
+import {combineReducers} from 'redux'
+
+// передаем объект конфигурации, описывающий все reducers
+export default combineReducers({
+
+})
